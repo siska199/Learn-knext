e361b8c258235a0aeba8311c66f52a3a3e20e8be
siska199 <siska.apriana.1703226@students.um.ac.id>
2022-06-18 08:39:15 +0700

    CRUD done

--- src/controllers/buku.js
+++ src/controllers/buku.js
@@ -1,8 +1,21 @@
+const { default: knex } = require("knex");
 const db = require("../config/db");
 
 exports.getBukus = async (req, res) => {
   try {
-    let buku = await db("buku");
+    const bukus = await db("buku");
+    res.status(200).json(bukus);
+  } catch (error) {
+    console.log("error: ", error);
+    res.status(500).send(`${error}`);
+  }
+};
+
+exports.getBuku = async (req, res) => {
+  try {
+    const { id } = req.params;
+    console.log("id: ", id);
+    const buku = await db("buku").where({ id }).select();
     res.status(200).json(buku);
   } catch (error) {
     console.log("error: ", error);
@@ -10,12 +23,33 @@ exports.getBukus = async (req, res) => {
   }
 };
 
-exports.addBuku = async(req,res)=>{
+exports.addBuku = async (req, res) => {
   try {
-    const {body} = req
-    let addData = await db('buku').insert(body)
-    res.status(200).send(`success add data with id ${addData[0]}`)
+    const { body } = req;
+    let addData = await db("buku").insert(body);
+    res.status(201).send(`success add data with id ${addData[0]}`);
   } catch (error) {
-    res.status(500).send(`${error}`)
+    res.status(500).send(`${error}`);
   }
-}
+};
+
+exports.updateBuku = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { body } = req;
+    const updateData = await db("buku").where({ id }).update(body);
+    res.status(201).json(`update data with id ${updateData[0]} success`);
+  } catch (error) {
+    res.status(500).send(`${error}`);
+  }
+};
+
+exports.deleteBuku = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteData = await db("buku").where({ id }).del();
+    res.status(200).json(`delete data with id ${deleteData} success`);
+  } catch (error) {
+    res.status(500).send(`${error}`);
+  }
+};
--- src/routers/index.js
+++ src/routers/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 
 const router = express.Router();
-const { getBukus, addBuku } = require("../controllers/buku.js");
+const { getBukus,getBuku, addBuku, updateBuku, deleteBuku } = require("../controllers/buku.js");
 
 router.get("/bukus", getBukus);
+router.get("/bukus/:id", getBuku);
 router.post("/buku", addBuku);
-router.put
+router.put("/bukus/:id",updateBuku)
+router.delete("/bukus/:id", deleteBuku)
 module.exports = router;
